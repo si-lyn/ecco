9c4c9f21de4b041729a0673ddd668ad96754d327
Zaber <54282299+si-lyn@users.noreply.github.com>
2025-11-07 17:19:20 +0100

    Added identifier to AttributeArtifactData which is only used for id and not to be written

--- adapter/rust/src/main/java/at/jku/isse/ecco/adapter/rust/data/AttributeArtifactData.java
+++ adapter/rust/src/main/java/at/jku/isse/ecco/adapter/rust/data/AttributeArtifactData.java
@@ -5,7 +5,10 @@ import lombok.Data;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.util.Objects;
 
 @Data
 public class AttributeArtifactData implements ArtifactData {
+    // String used to identify the attribute, not to write it
+    private final String attribute; // e.g., "#[derive(Debug)]", "#[test]", etc.
 }
