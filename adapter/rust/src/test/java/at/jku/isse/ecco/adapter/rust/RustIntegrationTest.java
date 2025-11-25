076388b34f4b49de4f74fe7a434d059f3e27cbc8
Zaber <54282299+si-lyn@users.noreply.github.com>
2025-11-25 23:15:55 +0100

    Refactor application test to use ParameterizedTest

--- adapter/rust/src/test/java/at/jku/isse/ecco/adapter/rust/RustIntegrationTest.java
+++ adapter/rust/src/test/java/at/jku/isse/ecco/adapter/rust/RustIntegrationTest.java
@@ -1,14 +1,18 @@
 package at.jku.isse.ecco.adapter.rust;
 
 import at.jku.isse.ecco.service.EccoService;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static at.jku.isse.ecco.adapter.rust.Utils.assertFilesEqual;
 import static at.jku.isse.ecco.adapter.rust.Utils.deleteDirectoryRecursively;
@@ -40,7 +44,7 @@ class RustIntegrationTest {
     // Disabled since comments are not supported by antlr parser
     @Disabled
     @Test
-    void comments() throws Exception {
+    void comments() {
             Path testFolder = Paths.get("src/test/resources/rust_examples/commentTest/");
             commitSingleDir(testFolder, service);
         try {
@@ -71,7 +75,7 @@ class RustIntegrationTest {
     }
 
     @Test
-    void struct() throws Exception {
+    void struct() {
         Path testFolder = Paths.get("src/test/resources/rust_examples/structTest/");
         commitSingleDir(testFolder, service);
         service.checkout("struct.1");
@@ -81,7 +85,7 @@ class RustIntegrationTest {
     }
 
     @Test
-    void MergeEnumTest() throws Exception {
+    void MergeEnumTest() {
         String[] folders = { "v1", "v2"};
         String testFolderStr = "src/test/resources/rust_examples/MergeEnumTest/";
         commit(folders, testFolderStr, service);
@@ -91,77 +95,82 @@ class RustIntegrationTest {
         assertFilesEqual(actual, testOutput);
     }
 
-    @Test
-    void applicationV1() throws  Exception {
-        String[] folders = { "v1"};
-        String testFolderStr = "src/test/resources/rust_examples/application/";
-        commit(folders, testFolderStr, service);
 
-        service.checkout("base.1,create.1,get.1");
-        Path actual = Paths.get("src/test/resources/rust_examples/application/v1/main.rs");
-        Path testOutput = Paths.get("src/test/resources/rust_examples/test_output/main.rs");
-        assertFilesEqual(actual, testOutput);
+    //  @TODO create v1, v3 results for application variants
+    private static Stream<Arguments> provideApplicationVariants() {
+        return Stream.of(
+                Arguments.of(new String[] { "v1" }, "base.1,create.1,get.1"),
+                Arguments.of(new String[] { "v2" }, "base.1,create.1,getAll.1"),
+                Arguments.of(new String[] { "v3" }, "base.1,create.1,get.1,updatePassword.1"),
+                Arguments.of(new String[] { "v1", "v2" }, "base.1,create.1,get.1,getAll.1"),
+                Arguments.of(new String[] { "v2", "v3" }, "create.1,getAll.1,updatePassword.1,base.1"),
+                Arguments.of(new String[] { "v1", "v3" }, "base.1,create.1,get.1,updatePassword.1"),
+                Arguments.of(new String[] { "v1", "v2", "v3" }, "create.1,get.1,getAll.1,change.1,base.1")
+        );
     }
 
-    @Test
-    void applicationV2() throws  Exception {
-        String[] folders = {"v2"};
+    /** Test various combinations of committing and checking out application variants
+     * @param folders Folders to commit
+     * @param checkoutConfig Configuration string for checkout
+     * @throws IOException
+     */
+    @ParameterizedTest
+    @MethodSource("provideApplicationVariants")
+    @DisplayName("Application Variants Commit and Checkout Test")
+    void applicationVariants(String[] folders,  String checkoutConfig) throws IOException {
         String testFolderStr = "src/test/resources/rust_examples/application/";
         commit(folders, testFolderStr, service);
+        String combinedFolders = String.join(",", folders);
+        Path actualLocation = Paths.get("src/test/resources/rust_examples/application/actual_checkout/"+combinedFolders);
+        deleteDirectoryRecursively(actualLocation);
+        Files.createDirectories(actualLocation);
 
-        service.checkout("base.1,create.1,getAll.1");
-        Path actual = Paths.get("src/test/resources/rust_examples/application/v2/main.rs");
-        Path testOutput = Paths.get("src/test/resources/rust_examples/test_output/main.rs");
-        assertFilesEqual(actual, testOutput);
-    }
+        service.setBaseDir(actualLocation.toAbsolutePath());
+        service.checkout(checkoutConfig);
+        Path expected;
+        if (folders.length == 1) {
+            expected = Paths.get("src/test/resources/rust_examples/application/" + folders[0] + "/main.rs");
+        } else {
+            String resultFolderName = "result" + String.join("", Arrays.stream(folders).map(f -> f.toUpperCase()).collect(Collectors.toList()));
+            expected = Paths.get("src/test/resources/rust_examples/application/results/" + resultFolderName + "/main.rs");
+        }
 
-    @Test
-    void applicationV3() throws  Exception {
-        String[] folders = { "v3"};
-        String testFolderStr = "src/test/resources/rust_examples/application/";
-        commit(folders, testFolderStr, service);
-
-        service.checkout("base.1,create.1,get.1,updatePassword.1");
-        Path actual = Paths.get("src/test/resources/rust_examples/application/v3/main.rs");
-        Path testOutput = Paths.get("src/test/resources/rust_examples/test_output/main.rs");
-        assertFilesEqual(actual, testOutput);
-    }
-
-    @Test
-    void applicationV1V2() throws  Exception {
-        String[] folders = { "v1", "v2"};
-        String testFolderStr = "src/test/resources/rust_examples/application/";
-        commit(folders, testFolderStr, service);
-
-        service.checkout("base.1,create.1,get.1,getAll.1");
-        Path actual = Paths.get("src/test/resources/rust_examples/application/results/resultV1V2/main.rs");
-        Path testOutput = Paths.get("src/test/resources/rust_examples/test_output/main.rs");
-        assertFilesEqual(actual, testOutput);
-    }
-
-    @Test
-    void applicationV2V3() throws  Exception {
-        String[] folders = { "v2", "v3"};
-        String testFolderStr = "src/test/resources/rust_examples/application/";
-        commit(folders, testFolderStr, service);
-
-        service.checkout("create.1,getAll.1,updatePassword.1,base.1");
-        Path actual = Paths.get("src/test/resources/rust_examples/application/results/resultV2V3/main.rs");
-        Path testOutput = Paths.get("src/test/resources/rust_examples/test_output/main.rs");
-        assertFilesEqual(actual, testOutput);
+        assertFilesEqual(actualLocation.resolve("main.rs"), expected);
     }
 
 
-    @Test
-    void applicationV1V2V3() throws  Exception {
+    //commit v1,v2,v3 and checkout v1,v2,v3 individually
+    @TestFactory
+    Stream<DynamicTest> commitAllVariantsOfApplicationAndCheckoutOneByOne() throws Exception {
         String[] folders = { "v1", "v2", "v3"};
-        String testFolderStr = "src/test/resources/rust_examples/application/";
-        commit(folders, testFolderStr, service);
+        Path testFolder = Path.of("src/test/resources/rust_examples/application/");
+        Path actualLocation = testFolder.resolve("actual_checkout");
+        deleteDirectoryRecursively(actualLocation);
+        Files.createDirectories(actualLocation);
+        commit(folders, testFolder.toString(), service);
+        List<String> configs = new ArrayList<>();
+        for (String folder : folders) {
+            List<String> lines = Files.readAllLines(testFolder.resolve(folder).resolve(".config"));
+            String combinedLines = String.join("", lines);
+            configs.add(combinedLines);
+        }
 
-        service.checkout("create.1,get.1,getAll.1,change.1,base.1");
-        Path actual = Paths.get("src/test/resources/rust_examples/application/results/resultV1V2V3/main.rs");
-        Path testOutput = Paths.get("src/test/resources/rust_examples/test_output/main.rs");
-        assertFilesEqual(actual, testOutput);
+        return Arrays.stream(folders).map(folder -> DynamicTest.dynamicTest("Checkout " + folder, () -> {
+            String checkoutConfig = switch (folder) {
+                case "v1" -> configs.get(0);
+                case "v2" -> configs.get(1);
+                case "v3" -> configs.get(2);
+                default -> throw new IllegalStateException("Unexpected value: " + folder);
+            };
+            Path dir = actualLocation.resolve(folder);
+            Files.createDirectories(dir);
+            service.setBaseDir(actualLocation.resolve(folder).toAbsolutePath());
+            service.checkout(checkoutConfig);
+            Path actual = actualLocation.resolve(folder).resolve("main.rs");
+            Path expected = testFolder.resolve(folder).resolve("main.rs");
+            assertFilesEqual(actual, expected);
+        }));
+
     }
 
     // Test for commiting a windows logger and a unix logger, and then extract the shared logic
